12411bffcbb9982085ad53b603389a524c0f1583
Heráclito <heraclitoqsaldanha@gmail.com>
2023-04-08 04:31:44 -0300

    add checked version function call

--- crates/host/src/func.rs
+++ crates/host/src/func.rs
@@ -10,15 +10,18 @@ pub struct Func<P: serde::ser::Serialize, R: serde::de::DeserializeOwned> {
 
 impl<P: serde::ser::Serialize, R: serde::de::DeserializeOwned> Func<P, R> {
 	pub fn call(&self, value: &P) -> R {
+		self.checked_call(value).unwrap()
+	}
+	pub fn checked_call(&self, value: &P) -> anyhow::Result<R> {
 		let RuntimeCaller { memory, alloc_fn, .. } = self.store.borrow().data().unwrap();
-		let buffer = serialize(value).unwrap();
+		let buffer = serialize(value)?;
 		let len = buffer.len() as _;
-		let ptr = alloc_fn.call(&mut *self.store.borrow_mut(), len).unwrap();
-		memory.write(&mut *self.store.borrow_mut(), ptr as _, &buffer).unwrap();
-		let ptr = self.wasm_fn.call(&mut *self.store.borrow_mut(), into_bitwise(ptr, len)).unwrap();
+		let ptr = alloc_fn.call(&mut *self.store.borrow_mut(), len)?;
+		memory.write(&mut *self.store.borrow_mut(), ptr as _, &buffer)?;
+		let ptr = self.wasm_fn.call(&mut *self.store.borrow_mut(), into_bitwise(ptr, len))?;
 		let (ptr, len) = from_bitwise(ptr);
 		let mut buffer = vec![0u8; len as _];
-		memory.read(& *self.store.borrow(), ptr as _, &mut buffer).unwrap();
-		deserialize(&buffer).unwrap()
+		memory.read(&*self.store.borrow(), ptr as _, &mut buffer)?;
+		Ok(deserialize(&buffer)?)
 	}
 }
